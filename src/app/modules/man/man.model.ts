f3e0d17c833d37d3ee47c0e12a63529004bfd35d
hasibhu <hasibul1987@gmail.com>
2024-11-18 15:17:56 +0100

    customised validator has been applied for name  in man.model.ts 2

--- src/app/modules/man/man.model.ts
+++ src/app/modules/man/man.model.ts
@@ -9,7 +9,15 @@ import validator from "validator";
 const manSchema = new Schema<Man>({
     name: {
         type: String,
-        required: true
+        trim: true,  //to remove unnecessary spaces
+        required: true,
+        validate: {             //cutomised validator
+            validator: function (value: string) {
+                const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1)
+                return firstNameStr === value
+            },
+            message: "{VALUE} is not in capitalized format. "
+        }
         
     },
     degree: {
