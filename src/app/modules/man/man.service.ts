cc5c1ecb651760c3e3c450a546094d6e46cafbd4
hasibhu <hasibul1987@gmail.com>
2024-11-20 22:30:32 +0100

    Bult in instance method of mongoose has been applied in the man.service.ts

--- src/app/modules/man/man.service.ts
+++ src/app/modules/man/man.service.ts
@@ -3,6 +3,15 @@ import manModel from "./man.model"
 
 
 export const manService = async (manData: Man) => {
-    const result = await manModel.create(manData) 
+    // const result = await manModel.create(manData) // this is a built in static method
+
+    // alternative
+
+    const resultInstance = new manModel(manData)
+
+    const result = await resultInstance.save(); //this is a built in instance method
+
+
+
     return result;
 }
