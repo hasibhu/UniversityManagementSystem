37abf64de049dc6790c57fccc848ac25ea398488
hasibhu <hasibul1987@gmail.com>
2024-11-30 22:32:13 +0100

    pre and post middleware of mongoose have been added on the student.module.ts

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -146,6 +146,15 @@ const studentSchema = new Schema<TStudent, StudentModel>(
   },
 );
 
+
+// Pre save middle/ hook 
+
+studentSchema.pre('save', function (next) {
+  console.log(this, 'Pre hook: we will save the data.');
+})
+
+
+
 // virtual
 studentSchema.virtual('fullName').get(function () {
   return this.name.firstName + this.name.middleName + this.name.lastName;
