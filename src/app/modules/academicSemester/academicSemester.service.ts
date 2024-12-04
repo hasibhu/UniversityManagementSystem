d743c17bd5bc9a1d7f3bc7b49d2039e114ef33ee
hasibhu <hasibul1987@gmail.com>
2024-12-04 10:19:44 +0100

    To block same year but two academic semester has been blocked in service logic of academicSemester module

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -3,6 +3,22 @@ import { AcademicSemesterModel } from "./academicSemester.model";
 
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+    
+    //semester name and semseter code check 
+    type TAcademisSemesterAndCodeMapper = {
+        [key: string]: string
+    }
+    const academisSemesterAndCodeMapper : TAcademisSemesterAndCodeMapper = {
+        Autumn: "01",
+        Summer: "02",
+        Fall: "03"
+    }
+    
+    if (academisSemesterAndCodeMapper[payload.name] !== payload.code) {
+        throw new Error('Invalid academic code.')
+    }
+    
+    
     const result = await AcademicSemesterModel.create(payload)
     
     return result;
