d0e496c8aae8ded050cc498112210ee963e986c4
hasibhu <hasibul1987@gmail.com>
2024-12-06 09:59:11 +0100

    customised app error provider has been added

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 
  const globalErrorHandle = (err: any, req: Request, res: Response, next: NextFunction) =>
 {
-  const statusCode = 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong';
 
   return res.status(statusCode).json({
--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TAcademicDepartment } from "./academicDepartment.interface";
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 
 
 
@@ -21,6 +23,45 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
         timestamps: true,
     }
 )
+
+
+
+
+
+
+
+
+
+
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartmentModel.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This department is already exist!',
+    );
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartmentModel.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This department does not exist! ',
+    );
+  }
+
+  next();
+});
+
 
 
 export const AcademicDepartmentModel = model<TAcademicDepartment>("AcademicDepartment",academicDepartmentSchema )
